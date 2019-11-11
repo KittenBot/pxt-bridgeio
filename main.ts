25231f2425c39620ac1cb04f3ea8fbe060abfb96
Riven <4771215@qq.com>
2019-11-11 17:33:02 +0800

    first working version

--- main.ts
+++ main.ts
@@ -7,45 +7,45 @@ load dependency
 //% color="#31C7D5" weight=10 icon="\uf1e6"
 namespace bridgeio {
 
-    const DigiPinMap= [
-        P0 = 
-    ]
+    export enum ArduinoDigi {
+        D0 = 150,
+        D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12,D13,
+        A0 = 100,
+        A1, A2, A3, A4, A5
+    }
+
+    export enum ArduinoAnalog {
+        A0 = 100,
+        A1,A2,A3,A4,A5
+    }
     
     //% blockId=_digiwrite block="%pin Digital Write %value"
     //% weight=100
-    export function DigiWrite(pin: DigitalInOutPin, value: boolean): void {
-        pin.digitalWrite(value);
+    export function DigiWrite(pin: ArduinoDigi, value: boolean): void {
+        pins.pinByCfg(pin).digitalWrite(value);
     }
 
     //% blockId=iobit_digiread block="%pin Digital Read"
     //% weight=98
-    export function DigiRead(pin: DigitalInOutPin): boolean {
-        return pin.digitalRead();
+    export function DigiRead(pin: ArduinoDigi): boolean {
+        return pins.pinByCfg(pin).digitalRead();
     }
 
     //% blockId=iobit_analogrd block="%pin Analog Read"
     //% weight=97
-    export function AnalogRead(pin: AnalogInPin): number {
-        return pin.analogRead();
+    export function AnalogRead(pin: ArduinoAnalog): number {
+        const p = pins.pinByCfg(pin) as AnalogInPin;
+        if (p)
+            return p.analogRead();
+        else
+            return 0;
     }
 
     //% blockId=iobit_analogwr block="%pin Analog Write %value"
     //% weight=96
-    export function AnalogWrite(pin: AnalogOutPin, value: number): void {
-        pin.analogWrite(value);
-    }
-
-    //% blockId=iobit_servo block="%pin Servo %degree °"
-    //% weight=95
-    export function Servo(pin: PwmPin, degree: number): void {
-        pin.servoWrite(degree);
-    }
-
-    //% blockId=iobit_pwm block="%pin Pwm %value Period %period ms"
-    //% weight=94
-    export function PWM(pin: PwmPin, value: number, period: number): void {
-        pin.analogSetPeriod(period);
-        pin.analogWrite(value);
+    export function AnalogWrite(pin: ArduinoAnalog, value: number): void {
+        const p = pins.pinByCfg(pin) as AnalogOutPin;
+        if (p) p.analogWrite(value);
     }
 
     
